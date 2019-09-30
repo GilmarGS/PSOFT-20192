05cd44b42e59dae5653f11c7866107884591bc16
Gilmargs <gilmar.silva@ccc.ufcg.edu.br>
2019-09-30 15:27:49 -0300

    corrigindo index

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,7 @@ function create_turma(disciplina, vagas){
     turma.alunos = [];
   
     turma.matricule = function(aluno){
-      if(turma.vagas >= 0){
+      if(turma.vagas > 0){
         turma.alunos.push(aluno);
         turma.vagas --;
         return true;
@@ -20,3 +20,7 @@ function create_turma(disciplina, vagas){
     }
   }
   
+  turma = create_turma("psfot", 2);
+  turma.matricule("gilmar");
+  turma.matricule("silva");
+  turma.matricule("da silva");
